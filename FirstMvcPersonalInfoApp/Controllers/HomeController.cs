bb665fb8a0c0e6b55af9d7a956ffea179fcd76f8
sakibmist <sakib.mist.cse1993@gmail.com>
2018-01-10 22:46:58 +0600

    mvc requirements updated

---
+++ FirstMvcPersonalInfoApp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMvcPersonalInfoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            ViewBag.msg = "Data Pass By Bag";
+            return View();
+        }
+    }
+}
